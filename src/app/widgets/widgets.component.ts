108dce451b244bf366d601c92623de9adcf06a5d
Mateus Guizelini <mateus.guizelini@tech.math.group>
2022-09-20 18:15:47 -0300

    ajuste para mostrar ou nao botao de sacar

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { RequestService } from 'src/app/requests/service/request.service';
+import { Router } from '@angular/router';
 
+import { RequestService } from 'src/app/requests/service/request.service';
 import { getValueComission, getStatusWaiting } from 'src/app/helper/global';
 
 @Component({
@@ -12,6 +13,9 @@ export class WidgetsComponent implements OnInit {
   sales!: any;
   salesTotal!: any;
   totalSales!: any;
+  countApproved: number = 0;
+  isApproved: boolean = false;
+  showButton: boolean = false;
 
   requests!: any;
   withdrawalsMade: number = 0;
@@ -20,16 +24,23 @@ export class WidgetsComponent implements OnInit {
   countRequestWaiting: number = 0;
 
   constructor(
-    private requestService: RequestService
+    private requestService: RequestService,
+    private router: Router,
   ) { }
 
   async ngOnInit(){
     this.requests = await this.requestService.getAll().toPromise();
     this.salesTotal = await this.requestService.getRequestStore('sum').toPromise();
     this.totalSales = this.salesTotal.salesApproved;
-    this.withdrawalsToBeMade = getValueComission(this.totalSales, this.requests);
-    this.countRequestWaiting = getStatusWaiting(this.requests);
+    this.countApproved = this.salesTotal.countApproved;
+    this.isApproved = this.salesTotal.isApproved;
+    this.withdrawalsToBeMade = await getValueComission(this.totalSales, this.requests);
+    this.countRequestWaiting = await getStatusWaiting(this.requests);
     this.withdrawalsToBeMadeFormat = this.withdrawalsToBeMade.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    
+    this.showButton = this.router.url.indexOf('/withdraw') > -1 && this.withdrawalsToBeMade > 0 && this.countRequestWaiting == 0 ? false : true;
+  }
+
+  withdraw(){
+    this.router.navigate(['/withdraw']);
   }
 }
