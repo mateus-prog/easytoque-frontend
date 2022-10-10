75ba514886be6c94f2116a55938490f311fd2cb4
Mateus Guizelini <mateus.guizelini@tech.math.group>
2022-10-10 18:36:49 -0300

    exclusao de funcoes desnecessarias

--- src/app/partners/service/partner_bank.service.ts
+++ src/app/partners/service/partner_bank.service.ts
@@ -16,38 +16,14 @@ export class PartnerBankService {
         private auth: AuthenticationService
     ) { }
 
-    getAll() {
-        return this.httpClient.get<IPartnerBank[]>(baseUrl, {
-            headers: this.auth.getAuthorizationHeader()
-        });
-    }
-
     getByUser(user_id: number) {
         return this.httpClient.get<IPartnerBank[]>(`${baseUrl}/${user_id}`, {
             headers: this.auth.getAuthorizationHeader()
         });
     }
 
-    getById(id: number) {
-        return this.httpClient.get<IPartnerBank>(`${baseUrl}/${id}`, {
-            headers: this.auth.getAuthorizationHeader()
-        });
-    }
-
-    create(partner: IPartnerBank) {
-        return this.httpClient.post<IPartnerBank>(baseUrl, partner, {
-            headers: this.auth.getAuthorizationHeader()
-        });
-    }
-
     update(id: number, partner: IPartnerBank) {
         return this.httpClient.put<IPartnerBank>(`${baseUrl}/${id}`, partner, {
-            headers: this.auth.getAuthorizationHeader()
-        });
-    }
-
-    delete(id: number) {
-        return this.httpClient.delete<void>(`${baseUrl}/${id}`, {
             headers: this.auth.getAuthorizationHeader()
         });
     }
--- src/app/partners/service/partner_corporate.service.ts
+++ src/app/partners/service/partner_corporate.service.ts
@@ -16,41 +16,11 @@ export class PartnerCorporateService {
         private auth: AuthenticationService
     ) { }
 
-    getAll() {
-        return this.httpClient.get<IPartnerCorporate[]>(baseUrl, {
-            headers: this.auth.getAuthorizationHeader()
-        });
-    }
-
     getByUser(user_id: number) {
         return this.httpClient.get<IPartnerCorporate[]>(`${baseUrl}/${user_id}`);
     }
 
     getByHash(hash_id: string) {
         return this.httpClient.get<IPartnerCorporate[]>(`${baseUrl}/${hash_id}/edit`);
-    }
-    
-    getById(id: number) {
-        return this.httpClient.get<IPartnerCorporate>(`${baseUrl}/${id}`, {
-            headers: this.auth.getAuthorizationHeader()
-        });
-    }
-
-    create(partner: IPartnerCorporate) {
-        return this.httpClient.post<IPartnerCorporate>(baseUrl, partner, {
-            headers: this.auth.getAuthorizationHeader()
-        });
-    }
-
-    update(id: number, partner: IPartnerCorporate) {
-        return this.httpClient.put<IPartnerCorporate>(`${baseUrl}/${id}`, partner, {
-            headers: this.auth.getAuthorizationHeader()
-        });
-    }
-
-    delete(id: number) {
-        return this.httpClient.delete<void>(`${baseUrl}/${id}`, {
-            headers: this.auth.getAuthorizationHeader()
-        });
     }
 }
